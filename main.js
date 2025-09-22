c2f76409c65732a7a418a77aafcd8a2cd4be0e44
PurpleAbi <abigail.santana.medina@gmail.com>
2025-09-22 23:03:16 +0200

    under construction

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 function burgerMenu() {
-    const navLinks = document.querySelector(".links");
-    if (navLinks.style.display === "flex") {
-      navLinks.style.display = "none";
-    } else {
-      navLinks.style.display = "flex";
-    }
+  const navLinks = document.querySelector(".links");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
   }
-  
-  const myAge = (new Date().getFullYear()) - 1995;
-  document.getElementById("my-age").innerText = myAge;
+}
+
+let i = 0;
+let txt = "You can check out my Virtual Resumee in the meantime";
+let speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeWriter);
+
+const myAge = (new Date().getFullYear()) - 1995;
+document.getElementById("my-age").innerText = myAge;
+
 
